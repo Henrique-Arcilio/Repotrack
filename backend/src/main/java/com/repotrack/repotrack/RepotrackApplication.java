df818026303dceff32ec071dce6ac97e2c9bffcc
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-07-03 14:43:51 -0300

    chore: estrutural inicial do projeto

---
+++ backend/src/main/java/com/repotrack/repotrack/RepotrackApplication.java
@@ -0,0 +1,13 @@
+package com.repotrack.repotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepotrackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/repotrack/repotrack/RepotrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repotrack.repotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepotrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
