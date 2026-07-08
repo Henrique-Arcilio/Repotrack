93b397514562fea153a6a3a95d75ce445e7bd03a
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-07-08 10:42:34 -0300

    refactor: corrigindo endpoint repotrack para o deploy no render

--- android/app/src/main/java/com/example/repotrack/data/remote/client/RepotrackClient.kt
+++ android/app/src/main/java/com/example/repotrack/data/remote/client/RepotrackClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RepotrackClient {
-    private const val BASE_URL = "http://192.168.18.94:8080/api/v1/"
+    private const val BASE_URL = "https://repotrack-wpk1.onrender.com/api/v1/"
 
     val api: RepotrackApi by lazy {
         Retrofit.Builder()
